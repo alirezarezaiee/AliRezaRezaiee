a17fa45dc68af734634df02bcf3eb5e5cf364718
Ali Reza Rezaiee <alirezarezaiee2022@gmail.com>
2025-09-18 22:29:52 +0430

    Add files via upload
    ok

---
+++ MathRandom.java
@@ -0,0 +1,17 @@
+
+package math.random;
+
+/**
+ *
+ * @author MFarjad
+ */
+public class MathRandom {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

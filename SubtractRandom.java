23733f1e5cc331fcfd8768ff22e3f338ddb266f9
Ali Reza Rezaiee <alirezarezaiee2022@gmail.com>
2025-09-19 11:25:38 +0430

    Add files via upload
    ok

---
+++ SubtractRandom.java
@@ -0,0 +1,27 @@
+
+package addrandom;
+
+import java.util.Scanner;
+
+
+
+public class subtractRandom {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        double num1 = (int)(Math.random() * 10);
+        double num2 = (int)(Math.random() * 10);
+        if (num1 < num2){
+        int temp =  (int) num1;
+            num1 =  num2;
+            num2 =  temp;
+        }
+        System.out.println((int)num1 + " - " + (int)num2 + " = ?");
+        double mainResult = num1 - num2;
+        double userResult = input.nextDouble();
+        if (mainResult == userResult)
+            System.out.println("true");
+        else
+            System.out.println("False");
+    }
+}
